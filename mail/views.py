cf00b642986d8d098839dc9b32375a0ad12c3987
Kamil Bednarski <kamilarturbednarski@gmail.com>
2020-12-30 19:11:18 +0100

    update:views.py register function error handler

--- mail/views.py
+++ mail/views.py
@@ -154,26 +154,31 @@ def logout_view(request):
 
 def register(request):
     if request.method == "POST":
-        email = request.POST["email"]
+        if request.POST['email'] and request.POST['password'] and request.POST['confirmation']:
+            email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "mail/register.html", {
-                "message": "Passwords must match."
-            })
+            # Ensure password matches confirmation
+            password = request.POST["password"]
+            confirmation = request.POST["confirmation"]
+            if password != confirmation:
+                return render(request, "mail/register.html", {
+                    "message": "Passwords must match."
+                })
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(email, email, password)
-            user.save()
-        except IntegrityError as e:
-            print(e)
+            # Attempt to create new user
+            try:
+                user = User.objects.create_user(email, email, password)
+                user.save()
+            except IntegrityError as e:
+                print(e)
+                return render(request, "mail/register.html", {
+                    "message": "Email address already taken."
+                })
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
             return render(request, "mail/register.html", {
-                "message": "Email address already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
+                    "message": "All fields must be filled."
+                })
     else:
         return render(request, "mail/register.html")
